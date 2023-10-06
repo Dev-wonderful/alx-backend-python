55f80098c5434d07d45a45eefd340b9519f02240
Dev-wonderful <adeonderful20@gmail.com>
2023-10-06 03:48:32 +0100

    convert to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""convert to tuple"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Convert to tuple"""
+    return k, v ** 2
