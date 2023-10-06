3c67eb50fbb6b5d10232be9cdc1fec3e56c41350
Dev-wonderful <adeonderful20@gmail.com>
2023-10-06 04:04:38 +0100

    decorator

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,3 +5,9 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """make multiplier"""
+    def fun(*args: float, multiplier):
+        """inside function"""
+        for arg in args:
+            sum = arg * multiplier
+        return sum
+    return fun
