ef2304323cb2620afbf3e5b8869c297ff77a6652
Dev-wonderful <adeonderful20@gmail.com>
2023-10-10 02:31:30 +0100

    co-routine

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,4 +10,14 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for _ in range(n):
         delay: float = await wait_random(max_delay)
         delay_list.append(delay)
-    return sorted(delay_list, key = lambda x:float(x))
+    length = len(delay_list)
+    for i in range(length):
+        swapped = False
+        for j in range(0, n-i-1):
+            if delay_list[j] > delay_list[j+1]:
+                delay_list[j], delay_list[j+1] = delay_list[j+1], delay_list[j]
+                swapped = True
+        if not swapped:
+            break
+
+    return delay_list
