f610c616ab641cac9a5661a6c7196e6c0e9a98cd
Dev-wonderful <adeonderful20@gmail.com>
2023-10-11 03:01:27 +0100

    fix: type annotation

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """measure runtime for async comprehension"""
-from typing import List
 import asyncio
 import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List:
+async def measure_runtime() -> float:
     """measure runtime for async comprehension in parallel"""
     start_time = time.time()
     await asyncio.gather(async_comprehension(), async_comprehension(),
